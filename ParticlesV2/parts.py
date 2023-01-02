f7fd2b544819170450dad903652a10685da1eec2
Charbel <charbel.kassab.08@gmail.com>
2023-01-02 14:31:45 -0500

    Add files via upload

---
+++ ParticlesV2/parts.py
@@ -0,0 +1,68 @@
+import pygame
+import random
+
+particules=[]
+window_size = 300
+pygame.init()
+window = pygame.display.set_mode((window_size, window_size))
+
+
+def draw(surface, x, y, color, size):
+    for i in range(0, size):
+        pygame.draw.line(surface, color, (x, y-1), (x, y+2), abs(size))
+               
+def particule(x, y, c):
+    return {"x": x, "y": y, "vx": 0, "vy": 0, "color": c}
+
+def randomxy():
+    return round(random.random()*window_size + 1)
+
+def create(number, color):
+    group = []
+    for i in range(number):
+        group.append(particule(randomxy(), randomxy(), color))
+        particules.append((group[i]))
+    return group
+
+def rule(particules1, particules2, g):
+    for i in range(len(particules1)):
+        fx = 0
+        fy = 0
+        for j in range(len(particules2)):
+            a = particules1[i]
+            b = particules2[j]
+            dx = a["x"] - b["x"]
+            dy = a["y"] - b["y"]
+            d = (dx*dx + dy*dy)**0.5
+            if( d > 0 and d < 80):
+                F = g/d
+                fx += F*dx
+                fy += F*dy
+        a["vx"] = (a["vx"] + fx)*0.5
+        a["vy"] = (a["vy"] + fy)*0.5
+        a["x"] += a["vx"]
+        a["y"] += a["vy"]
+        if(a["x"] <= 0 or a["x"] >= window_size):
+            a["vx"] *=-1
+        if(a["y"] <= 0 or a["y"] >= window_size):
+            a["vy"] *=-1        
+
+
+yellow = create(100, "yellow")
+red = create(100, "red")
+
+run = True
+while run:
+    window.fill(0)
+    rule(red, red, 0.1)
+    rule(red, yellow, -0.15)
+    rule(yellow, yellow, -0.1)
+    for i in range(len(particules)):
+        draw(window,  particules[i]["x"], particules[i]["y"], particules[i]["color"], 3)
+        
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.flip()
+pygame.quit()
+exit()
